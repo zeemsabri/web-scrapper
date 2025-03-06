24e64b1faf3afbda8027886854f5a5a5dd6b69d2
Muhammad Haseeb <50395046+Haseeb03@users.noreply.github.com>
2025-03-06 22:47:59 +0500

    Update api.php - add route to check job status
    Add route to check job status

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,9 @@ use App\Http\Controllers\OrganizationController;
 Route::post('/get-token', [TokenController::class, 'generateToken']);
 Route::get('/verify-token', [TokenController::class, 'verifyToken']);
 
-Route::middleware(\App\Http\Middleware\JWTMiddleware::class)->group(function () {
+Route::middleware(\App\Http\Middleware\JwtMiddleware::class)->group(function () {
    Route::post('/products', [ProductController::class, 'store']); 
    Route::post('/add_job', [OrganizationController::class, 'store']);
+   Route::post('/job_exists', [OrganizationController::class, 'job_exists']);
 
-}); 
-
-
-
-
+});
